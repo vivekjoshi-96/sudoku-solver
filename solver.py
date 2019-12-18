522e7363548ce7bbce965c9a3faef393874b1d3b
vivek joshi <reachvivekjoshi@gmail.com>
2019-12-18 23:30:15 +0530

    A sample board

--- solver.py
+++ solver.py
@@ -6,3 +6,23 @@ A valid row/column has values from 1-9 and no two cells have the same values
 Box is a smaller 3X3 grid, there are three boxes on one board
 A valid box also contains values from 1-9 and all cells have unique values
 """
+
+
+def main():
+    board = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
+    print(*board, sep='\n')
+
+
+if __name__ == '__main__':
+    main()
+
