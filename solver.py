1600e7d3dbaf9d6da848d8e994ac33f5b4782ed8
vivek joshi <reachvivekjoshi@gmail.com>
2019-12-18 23:23:48 +0530

    Rules for sudoku

---
+++ solver.py
@@ -0,0 +1,8 @@
+"""
+A board is a 9 X 9 grid
+Each cell contains a value from 0-9
+The board is Represented using a list of lists(9)
+A valid row/column has values from 1-9 and no two cells have the same values
+Box is a smaller 3X3 grid, there are three boxes on one board
+A valid box also contains values from 1-9 and all cells have unique values
+"""
